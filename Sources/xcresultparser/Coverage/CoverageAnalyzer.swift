c9f78344c4cc7d69db24fd4d741902a47a7d8c2d
Kasper Weibel Nielsen-Refs <kasper@vivino.com>
2022-01-20 22:45:53 +0100

    Add docs and clean up

--- Sources/xcresultparser/Coverage/CoverageAnalyzer.swift
+++ Sources/xcresultparser/Coverage/CoverageAnalyzer.swift
@@ -36,6 +36,14 @@ class CoverageAnalyzer {
 
     // MARK: Internal
 
+    /**
+      Analyze the coverage for the files
+
+      The returned list is sorted by the directory with the biggest number of uncovered lines. See the test case for an example
+      In sequence the first items have the largest potential for coverage improvement
+
+      - Returns: A list of CoverageAnalysisLine with a recursive sum of executableLines and coveredLines in the directories in the filessystem.
+     */
     func analyze() -> [CoverageAnalysisLine] {
         let missinglines = countMissingLines()
         let tally = tallyDirectories(lines: missinglines)
@@ -47,10 +55,14 @@ class CoverageAnalyzer {
 
     private let files: [CodeCoverageFile]
 
+    /**
+      Sum executableLines and coveredLines per directory
+
+      - Returns: An Array with a CoverageAnalysisLine per directory in the files sorted by the path string
+     */
     private func countMissingLines() -> [CoverageAnalysisLine] {
         var result: OrderedDictionary<URL, CoverageAnalysisLine> = [:]
-        let sortedFiles = files.sorted { $0.path.localizedStandardCompare($1.path) == .orderedAscending }
-        for file in sortedFiles {
+        for file in files {
             let path = URL(fileURLWithPath: file.path).deletingLastPathComponent()
             let def = CoverageAnalysisLine(path: path)
             result.updateValue(forKey: path, default: def) { value in
@@ -61,6 +73,13 @@ class CoverageAnalyzer {
         return Array(result.values)
     }
 
+    /**
+      Recursively Sum executableLines and coveredLines per directory
+
+      - Parameter lines: An Array of CoverageAnalysisLine
+
+      - Returns: An Array with a CoverageAnalysisLine per directory in the filesystem
+     */
     private func tallyDirectories(lines: [CoverageAnalysisLine]) -> [CoverageAnalysisLine] {
         var result: OrderedDictionary<[String], CoverageAnalysisLine> = [:]
         for line in lines {
@@ -79,6 +98,14 @@ class CoverageAnalyzer {
         return Array(result.values)
     }
 
+    /**
+      Sort by the directory with the biggest number of uncovered lines. See the test case for an example
+      When printed in sequence the top items have the largest potential for coverage improvement
+
+      - Parameter lines: An Array of CoverageAnalysisLine
+
+      - Returns: An Array with a CoverageAnalysisLine per directory sorted by the directory with the biggest number of uncovered lines
+     */
     private func sortByMissing(lines: [CoverageAnalysisLine]) -> [CoverageAnalysisLine] {
         var result: [CoverageAnalysisLine] = []
         let lookup: [[String]: Int] = lines.reduce(into: [[String]: Int]()) {
@@ -94,8 +121,14 @@ class CoverageAnalyzer {
                 key1.append(lhComp[idx])
                 key2.append(rhComp[idx])
                 idx += 1
-            } while lhComp[idx - 1] == lhComp[idx - 1] && (idx < lhComp.count && idx < rhComp.count)
-            return lookup[key1] ?? 0 > lookup[key2] ?? 0
+            } while lhComp[idx - 1] == rhComp[idx - 1] && (idx < lhComp.count && idx < rhComp.count)
+            let lookup1 = lookup[key1] ?? 0
+            let lookup2 = lookup[key2] ?? 0
+            if lookup1 == lookup2 {
+                return key1.joined().localizedStandardCompare(key2.joined()) == .orderedDescending
+            } else {
+                return lookup[key1] ?? 0 > lookup[key2] ?? 0
+            }
         }
         return result
     }
--- Tests/XcresultparserTests/CoverageAnalyzerTests.swift
+++ Tests/XcresultparserTests/CoverageAnalyzerTests.swift
@@ -16,6 +16,8 @@ final class CoverageAnalyzerTests: XCTestCase {
         var result: [CodeCoverageFile] = []
         var file = CodeCoverageFile(coveredLines: 10, lineCoverage: 1, path: "/a/c/x.swift", name: "", executableLines: 200, functions: [])
         result.append(file)
+        file = CodeCoverageFile(coveredLines: 99, lineCoverage: 1, path: "/a/b/e/g/y.swift", name: "", executableLines: 100, functions: [])
+        result.append(file)
         file = CodeCoverageFile(coveredLines: 5, lineCoverage: 1, path: "/a/b/d/e/x.swift", name: "", executableLines: 20, functions: [])
         result.append(file)
         file = CodeCoverageFile(coveredLines: 5, lineCoverage: 1, path: "/a/c/y.swift", name: "", executableLines: 20, functions: [])
@@ -26,6 +28,8 @@ final class CoverageAnalyzerTests: XCTestCase {
         result.append(file)
         file = CodeCoverageFile(coveredLines: 5, lineCoverage: 1, path: "/a/b/e/f/y.swift", name: "", executableLines: 100, functions: [])
         result.append(file)
+        file = CodeCoverageFile(coveredLines: 5, lineCoverage: 1, path: "/a/a/x.swift", name: "", executableLines: 210, functions: [])
+        result.append(file)
         return result
     }
 
@@ -33,7 +37,7 @@ final class CoverageAnalyzerTests: XCTestCase {
         let analyzer = CoverageAnalyzer(files: files)
         let result = analyzer.analyze()
         let expect = """
-[["file:///": 340], ["file:///a/": 340], ["file:///a/c/": 205], ["file:///a/b/": 135], ["file:///a/b/e/": 95], ["file:///a/b/e/f/": 95], ["file:///a/b/d/": 15], ["file:///a/b/d/e/": 15]]
+[["file:///": 546], ["file:///a/": 546], ["file:///a/a/": 205], ["file:///a/c/": 205], ["file:///a/b/": 136], ["file:///a/b/e/": 96], ["file:///a/b/e/f/": 95], ["file:///a/b/e/g/": 1], ["file:///a/b/d/": 15], ["file:///a/b/d/e/": 15]]
 """
         XCTAssertEqual(expect, result.map { ["\($0.path.absoluteString)": $0.missedLines] }.description)
     }
